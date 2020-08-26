36ccbd060193ced5b1d72e259446f2767c47b59d
Rinkan Rohit Jena <49729884+rinkanrohitjena@users.noreply.github.com>
2020-08-26 09:40:01 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+echo " The name of script is  $0"
+echo " The total number of argument passed is $# "
+echo " The exit status of the script is $?"
+echo " The process id of the script is  $$"
+echo " The user running the Script is $USER"
+echo " The host name of the Machine is $HOSTNAME "
+echo " The Arguments passed are $@"
+
+
+
+
